4c637a4b39611018738d1495a02cb8fc7ba61f21
dory <empty@empty>
2016-08-26 13:02:04 +0900

    add tutofile 	new file:   pointers.go

---
+++ pointers.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+func pointer_1() {
+	fmt.Println("pointer_1")
+	i, j := 42,	2701
+
+	p := &i
+	fmt.Println("&p = ", &p)
+	fmt.Println("p  = ", p)
+	fmt.Println("&i = ", &i)
+	fmt.Println(*p)
+
+	fmt.Println()
+
+	p = &j
+	fmt.Println("&p = ", &p)
+	fmt.Println("p  = ", p)
+	fmt.Println("&j = ", &j)
+	fmt.Println(*p)
+	return
+}
+
+func pointer_2() {
+	fmt.Println("pointer_2")
+	i, j := 42,	2701
+
+	p := &i
+	fmt.Println(*p)
+	*p = 21
+	fmt.Println(i)
+
+	fmt.Println()
+
+	p = &j
+	*p = *p / 37
+	fmt.Println(j)
+
+	return
+}
+
+func main() {
+
+	pointer_1()
+	fmt.Println()
+	pointer_2()
+
+}

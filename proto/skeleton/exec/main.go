193f93162fbb18eb1eab08d87d58f8c73f82ddca
dorymint <sk.dorymint@gmail.com>
2017-01-05 10:26:27 +0900

    fix  Changes to be committed: 	modified:   proto/skeleton/exec/main.go

--- proto/skeleton/exec/main.go
+++ proto/skeleton/exec/main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
+	"sync"
 )
 
 // exec test
 
 func main() {
-	execName()
+	//execName()
+	//execStart()
+	execStream(os.Stdin, os.Stdout, os.Stderr)
 }
 
 func execName() {
 	fmt.Println("test2")
+	// exit absolute file path
 	fmt.Println(exec.LookPath("ls"))
 	exe := exec.Command("ls")
 
@@ -29,4 +35,77 @@ func execName() {
 		log.Fatal(err)
 	}
 	fmt.Println(string(exist2))
+}
+
+func execStart() {
+	cmd := exec.Command("sleep", "5")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Waiting for command to finish ...")
+	err = cmd.Wait()
+	log.Printf("Command finished with error: %v\n", err)
+}
+
+func execStream(src io.Reader, dst io.Writer, errDst io.Writer) error {
+	cmd := exec.Command("tr", "a-z", "A-Z")
+	stdin, err := cmd.StdinPipe()
+	if err !=nil {
+		log.Fatal(err)
+	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// stdin
+	go func() {
+		stdin.Write([]byte("hello "))
+//		_, err = io.Copy(stdin, src)
+//		fmt.Println("copy to stdin")
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+		stdin.Write([]byte("world\n"))
+		stdin.Close()
+		fmt.Println("close stdin")
+		stdin.Write([]byte("hi!!!"))
+		wg.Done()
+	}()
+	// stdout
+	go func() {
+		_, err = io.Copy(dst, stdout)
+		fmt.Println("copy to os.stdout")
+		if err != nil {
+			log.Fatal(err)
+		}
+		stdout.Close()
+		fmt.Println("close stdout")
+		wg.Done()
+	}()
+	// stderr
+	go func() {
+		_, err = io.Copy(errDst, stderr)
+		fmt.Println("copy to os.stderr")
+		if err != nil {
+			log.Fatal(err)
+		}
+		stderr.Close()
+		fmt.Println("close stderr")
+		wg.Done()
+	}()
+	wg.Wait()
+	fmt.Println("wg done!")
+	return cmd.Wait()
 }

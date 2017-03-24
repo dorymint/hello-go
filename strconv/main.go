12e19c2a230bf6e7d7a4daaa6dc34c3c209a8211
dorymint <sk.dorymint@gmail.com>
2017-03-24 13:27:41 +0900

    nyan  Changes to be committed: 	new file:   etc/divition/main.go 	new file:   strconv/main.go

---
+++ strconv/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+func main() {
+	str := "128"
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v: %d\n", reflect.TypeOf(i), i*2)
+}

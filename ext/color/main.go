f836ea41303cf2a46f6d0b29c340bf6f2dcb377d
kamisari <kamisari10yonaga@gmail.com>
2017-05-24 15:13:40 +0900

    up color  Changes to be committed: 	modified:   main.go

--- ext/color/main.go
+++ ext/color/main.go
@@ -20,4 +20,31 @@ func main() {
 	color.Red("hello")
 
 	fmt.Fprintln(os.Stdout, color.MagentaString("hello string"))
+
+	fmt.Println("color list")
+	fns := []func(string, ...interface{}){
+		func(s string, i ...interface{}) { fmt.Println("----- Colors -----") },
+		color.Black,
+		color.Red,
+		color.Green,
+		color.Yellow,
+		color.Blue,
+		color.Magenta,
+		color.Cyan,
+		color.White,
+
+		func(s string, i ...interface{}) { fmt.Println("----- Hi Colors -----") },
+		color.HiBlack,
+		color.HiRed,
+		color.HiGreen,
+		color.HiYellow,
+		color.HiBlue,
+		color.HiMagenta,
+		color.HiCyan,
+		color.HiWhite,
+	}
+
+	for _, f := range fns {
+		f("colors !!")
+	}
 }

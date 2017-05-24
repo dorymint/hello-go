1ce859dc3be647b2403c42c3d64f9b807a5646da
kamisari <kamisari10yonaga@gmail.com>
2017-05-24 15:00:30 +0900

    up color  Changes to be committed: 	modified:   main.go

--- ext/color/main.go
+++ ext/color/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/fatih/color"
+	"os"
 )
+
+var flagColor = flag.Bool("color", false, "")
+
+func init() {
+	flag.Parse()
+	color.NoColor = *flagColor
+}
 
 func main() {
 	color.Cyan("hello")
+	color.Blue("hello")
+	color.Red("hello")
+
+	fmt.Fprintln(os.Stdout, color.MagentaString("hello string"))
 }

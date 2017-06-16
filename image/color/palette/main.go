66f3c0388c415edef90fa8e88592d8e1532879b0
kamisari <kamisari10yonaga@gmail.com>
2017-06-16 17:45:25 +0900

    add example for color  Changes to be committed: 	modified:   main.go

--- image/color/palette/main.go
+++ image/color/palette/main.go
@@ -2,39 +2,103 @@ package main
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"image"
+	"image/color"
 	"image/color/palette"
 	"image/png"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
-	out := flag.String("out", "img.png", "specify output file path")
-	flag.Parse()
-	if flag.NArg() != 0 {
-		log.Fatal("invalid argument:", flag.Args())
+	write := func(f *os.File, cp []color.Color) error {
+		length := len(cp)
+		img := image.NewNRGBA(image.Rect(0, 0, length, length))
+		for y := 0; y < length; y++ {
+			c := cp[y]
+			for x := 0; x < length; x++ {
+				img.Set(x, y, c)
+			}
+		}
+		var b []byte
+		buf := bytes.NewBuffer(b)
+		if err := png.Encode(buf, img); err != nil {
+			return err
+		}
+		if _, err := fmt.Fprint(f, buf); err != nil {
+			return err
+		}
+		return nil
 	}
-	f, err := os.Create(*out)
-	if err != nil {
+
+	writeFile := func(path string, cp []color.Color) error {
+		f, err := os.Create(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		if err := write(f, cp); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := writeFile("websafe.png", palette.WebSafe); err != nil {
 		log.Fatal(err)
+	}
+	if err := writeFile("plan9.png", palette.Plan9); err != nil {
+		log.Fatal(err)
+	}
+
+	var pl []color.Color
+	//pl = palette.WebSafe
+	pl = []color.Color{color.RGBA{0xff, 0xff, 0xff, 0xff}}
+	if err := writeFile("img.png", pl); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := whileMod("while.png", palette.WebSafe); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func whileMod(path string, cp []color.Color) error {
+	f, err := os.OpenFile(path, os.O_RDWR, 0666)
+	if err != nil {
+		return err
 	}
 	defer f.Close()
 
-	length := len(palette.WebSafe)
+	length := len(cp)
 	img := image.NewNRGBA(image.Rect(0, 0, length, length))
-	for y := 0; y < length; y++ {
-		c := palette.WebSafe[y]
-		for x := 0; x < length; x++ {
-			img.Set(x, y, c)
+	for i := 1; i < 10; i++ {
+		f.Truncate(0)
+		for y := 0; y < length; y++ {
+			c := cp[y]
+			for x := 0; x < length; x++ {
+				if i%2 == 0 {
+					img.Set(x, y, c)
+				} else {
+					img.Set(y, x, c)
+				}
+			}
 		}
+		var b []byte
+		buf := bytes.NewBuffer(b)
+		if err := png.Encode(buf, img); err != nil {
+			return err
+		}
+		if _, err := fmt.Fprint(f, buf); err != nil {
+			return err
+		}
+
+		buf.Reset()
+		if _, err := f.Seek(0, 0); err != nil {
+			return err
+		}
+		time.Sleep(time.Second * 2)
 	}
-	var b []byte
-	buf := bytes.NewBuffer(b)
-	if err := png.Encode(buf, img); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Fprint(f, buf)
+	return nil
 }

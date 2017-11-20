efe976a6445b7bc7f1624bfa6f0f0b2a1bc0f8d8
kamisari <kamisari10yonaga@gmail.com>
2017-11-20 13:43:38 +0900

    fix filepath  Changes to be committed: 	renamed:    abs/abs_test.go -> abs_test.go 	modified:   join_test.go 	deleted:    main.go

--- path/filepath/join_test.go
+++ path/filepath/join_test.go
@@ -8,4 +8,8 @@ import (
 func TestJoin(t *testing.T) {
 	t.Log(filepath.Join("", "hello.txt"))
 	t.Log(filepath.Join("", ".hello"))
+
+	in := filepath.Join("", ".hello")
+	out, err := filepath.Abs(in)
+	t.Log("in:", in, "out:", out, "err:", err)
 }

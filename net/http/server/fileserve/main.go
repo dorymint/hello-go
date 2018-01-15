baf55b8e85e655f67724f0749417abdc95d52594
kamisari <kamisari10yonaga@gmail.com>
2018-01-15 23:27:27 +0900

    up  Changes to be committed: 	new file:   net/http/server/fileserve/doc/index.html 	new file:   net/http/server/fileserve/doc/t/hello.txt 	new file:   net/http/server/fileserve/main.go 	new file:   regexp/: 	modified:   regexp/match_test.go

---
+++ net/http/server/fileserve/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func logging(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RemoteAddr, r.RequestURI)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	const root = "./doc"
+
+	port := flag.String("port", "localhost:8080", "listen port")
+	flag.Parse()
+	if flag.NArg() != 0 {
+		log.Fatal("invalid argument:", flag.Args())
+	}
+
+	http.Handle("/", logging(http.FileServer(http.Dir(root))))
+	log.Fatal(http.ListenAndServe(*port, nil))
+}

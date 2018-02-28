35a716018bf4160be7df913df4b43c598affb84f
kamisari <kamisari10yonaga@gmail.com>
2018-02-28 18:47:50 +0900

    up pkg/strings

--- pkg/strings/fields_test.go
+++ pkg/strings/fields_test.go
@@ -7,8 +7,11 @@ import (
 
 func TestFields(t *testing.T) {
 	s1 := "cmdname args1 args2"
-	t.Log(s1,strings.Fields(s1))
+	t.Logf("%s: %#v\n", s1, strings.Fields(s1))
 
 	s2 := " cmdname  args1	args2		"
-	t.Log(s2,strings.Fields(s2))
+	t.Logf("%s: %#v\n", s2, strings.Fields(s2))
+
+	s3 := "cmd \"space test\" arg"
+	t.Logf("%s: %#v\n", s3, strings.Fields(s3))
 }

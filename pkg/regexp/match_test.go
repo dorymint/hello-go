7586f005ea978fb37c8aa89f0ea042ed6cbbc25f
kamisari <kamisari10yonaga@gmail.com>
2018-03-17 00:14:40 +0900

    up regexp

--- pkg/regexp/match_test.go
+++ pkg/regexp/match_test.go
@@ -23,3 +23,28 @@ func TestFind(t *testing.T) {
 	t.Log(loc)
 	t.Log(str[loc[0]:loc[1]])
 }
+
+func TestGroup(t *testing.T) {
+	reg := "^hello-(?:world|lily)$"
+	match := regexp.MustCompile(reg)
+	tests := []struct {
+		str     string
+		ismatch bool
+	}{
+		{str: "hello world", ismatch: false},
+		{str: "hello-world", ismatch: true},
+		{str: "hello-lily", ismatch: true},
+		{str: "hello-", ismatch: false},
+		{str: "hello-wo", ismatch: false},
+		{str: "hello", ismatch: false},
+		{str: "world", ismatch: false},
+	}
+	t.Logf("reg:%v", reg)
+	for i, test := range tests {
+		if b := match.MatchString(test.str); b != test.ismatch {
+			t.Errorf("[Error %d]: unexpected result: reg:%v str:%v ismatch:%v bool:%v", i, reg, test.str, test.ismatch, b)
+		} else if b {
+			t.Logf("[Log %d]: matched:%v", i, test.str)
+		}
+	}
+}

0213bb9191f28d2ede9beab3842d83298a720bf2
yaeshimo <hatsushimoyae@gmail.com>
2019-03-07 22:25:02 +0900

    fix for

---
+++ base/for/range/itr/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ss := []string{"hello", "gopher", "world"}
+	for i, s := range ss {
+		fmt.Println(i, s)
+	}
+}
---
+++ base/for/range/pointer/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type val struct {
+	name string
+	age  int
+}
+
+var input = []val{
+	{name: "hello", age: 10},
+	{name: "gopher", age: 11},
+	{name: "world", age: 12},
+}
+
+const Expected = "gopher"
+
+func miss(vs []val) (p *val) {
+	for _, v := range vs {
+		if v.name == Expected {
+			p = &v
+		}
+	}
+	return p
+}
+
+func correct(vs []val) (p *val) {
+	for i, v := range vs {
+		if v.name == Expected {
+			p = &vs[i]
+		}
+	}
+	return p
+}
+
+func main() {
+	fmt.Printf("expected name: %q\n\n", Expected)
+
+	fmt.Println("miss:")
+	fmt.Printf("%+v\n\n", miss(input))
+
+	fmt.Println("correct:")
+	fmt.Printf("%+v\n\n", correct(input))
+}
